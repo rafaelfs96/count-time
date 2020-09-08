a3de5793cfd38ecc5428d4acc79536a2ba98ab98
rafaelfs96 <rafaelfs96@outlook.com>
2020-09-08 20:39:06 -0300

    change quotes

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -12,10 +12,10 @@ export default props => {
   return (
     <div className='datepicker'>
       <label htmlFor={ props.id }>{ props.text }: </label>
-      <input type="datetime-local" disabled={ props.useNow } name={ props.id }
+      <input type='datetime-local' disabled={ props.useNow } name={ props.id }
         id={ props.id } value={ props.value } onChange={ dateChangeHandler }/>
       <label htmlFor={ `use-now_${props.id}` }>Usar data atual?</label>
-      <input type="checkbox" name={ `use-now_${props.id}` } id={ `use-now_${props.id}` }
+      <input type='checkbox' name={ `use-now_${props.id}` } id={ `use-now_${props.id}` }
         value={ props.useNow } onChange={ useNowChangeHandler }/>
     </div>
   )
