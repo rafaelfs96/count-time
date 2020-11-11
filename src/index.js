795b54c2ee4f1ff4d12f14c8c44e1a8691f73c0e
rafaelfs96 <rafaelfs96@outlook.com>
2020-11-11 09:15:38 -0300

    final configs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import './reset.scss'
-import './base.scss'
-import './theme.scss'
+import './styles/app.scss'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './components/App'
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import reportWebVitals from './reportWebVitals'
+
 ReactDOM.render(<App />, document.getElementById('root'))
+
+serviceWorkerRegistration.register()
+reportWebVitals()
