4f0d4442b6b2455119dc5b77a65cc2b75a7e6cf9
rafaelfs96 <rafaelfs96@outlook.com>
2021-05-23 21:22:06 -0300

    add creation year to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,10 +3,11 @@ import './Footer.scss'
 import React from 'react'
 
 function Footer() {
+  const createdYear = 2020
   const currentYear = new Date().getFullYear()
   return (
     <footer className='Footer'>
-      © {currentYear} Made by <a className='link' rel='noreferrer' href="https://github.com/rafaelfs96" target='_blank'>rafaelfs96</a>
+      © {createdYear} - {currentYear} Made by <a className='link' rel='noreferrer' href="https://github.com/rafaelfs96" target='_blank'>rafaelfs96</a>
     </footer>
   )
 }
